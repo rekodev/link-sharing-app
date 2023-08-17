e16217e977298a2ca81d12b2dce48b9f8761e7a0
zadr0tishe <112801448+zadr0tishe@users.noreply.github.com>
2023-08-17 21:36:24 +0300

    created CreateAccount page, styled for mobile

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -4,12 +4,15 @@ import Home from './pages/Home';
 import Preview from './pages/Preview';
 import Profile from './pages/Profile';
 import Login from './pages/Login';
+import CreateAccount from './pages/CreateAccount';
 
 const RoutesComponent = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Login />} />
+        <Route path='/create-account' element={<CreateAccount />} />
+
         <Route element={<Layout />}>
           <Route path='/links' element={<Home />} />
           <Route path='/preview' element={<Preview />} />
---
+++ src/pages/CreateAccount/style.ts
@@ -0,0 +1,62 @@
+import styled from 'styled-components';
+import { themeColors } from '../../styles/Theme';
+
+export const StyledCreateAccount = styled.section`
+  padding: 2rem;
+`;
+
+export const StyledCreateAccountContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  > p {
+    margin-top: 0.5rem;
+    color: ${themeColors.darkGray};
+  }
+`;
+
+export const StyledLogoWrapper = styled.div`
+  height: 2.5rem;
+  margin-bottom: 4rem;
+  width: 180px;
+
+  img {
+    height: 100%;
+  }
+`;
+
+export const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 1.5rem;
+  margin-bottom: 1.5rem;
+  margin-top: 2.5rem;
+
+  button {
+    width: 100%;
+  }
+`;
+
+export const StyledAccountCreationTextWrapper = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+
+  p {
+    color: ${themeColors.darkGray};
+  }
+
+  a {
+    color: ${themeColors.indigo};
+
+    &:hover {
+      color: ${themeColors.lavender};
+    }
+  }
+`;
+
+export const StyledPasswordDisclaimer = styled.p`
+  font-size: 0.75rem;
+  color: ${themeColors.darkGray};
+`;
