ca522a7384ab1e2ceadf404d8d3bd2c713374fdb
zadr0tishe <112801448+zadr0tishe@users.noreply.github.com>
2023-08-25 14:23:59 +0300

    fixed server folder paths

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,6 @@
 import express, { Express } from 'express';
 import cors from 'cors';
-import dbConnection from '../config/db';
+import dbConnection from './config/db';
 
 const app: Express = express();
 
