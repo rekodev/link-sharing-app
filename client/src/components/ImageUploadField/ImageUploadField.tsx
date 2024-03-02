f435c844e55e75460511dc6f25d5e4baae4f8b6b
rekodev <112801448+zadr0tishe@users.noreply.github.com>
2024-03-02 16:32:05 +0200

    move ImageUpload to ImageUploadField

--- client/src/components/ImageUploadField/ImageUploadField.tsx
+++ client/src/components/ImageUploadField/ImageUploadField.tsx
@@ -1,12 +1,19 @@
-import { Dispatch, SetStateAction } from 'react';
+import { Alert, Snackbar } from '@mui/material';
+import { Dispatch, SetStateAction, useContext, useRef, useState } from 'react';
 
 import {
+  StyledImageUpload,
+  StyledImageUploadWrapper,
+  StyledPreImageUploadWrapper,
+  StyledUploadedImage,
   StyledCardHeading,
   StyledProfilePictureCard,
   UploadedImageWrapper,
 } from './style';
+import uploadImageIcon from '../../assets/images/icon-upload-image.svg';
+import { ProfileDetailsContext } from '../../contexts/profileDetailsContext';
 import { ProfilePicture } from '../../types/profileDetails';
-import ImageUpload from '../ImageUpload';
+import Svg from '../Svg';
 
 type Props = {
   imageData: ProfilePicture;
@@ -14,6 +21,108 @@ type Props = {
 };
 
 const ImageUploadField = ({ imageData, setImageData }: Props) => {
+  const [error, setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const { setProfileDetails } = useContext(ProfileDetailsContext);
+
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files && e.target.files[0];
+    console.log(file);
+
+    if (!file) return;
+
+    if (file.type === 'image/png' || file.type === 'image/jpeg') {
+      const img = new Image();
+
+      img.onload = () => {
+        if (img.width <= 1200 && img.height <= 1200) {
+          const reader = new FileReader();
+
+          reader.onload = (e: ProgressEvent<FileReader>) => {
+            if (!(e.target && e.target.result)) return;
+
+            setImageData({ name: file.name, id: e.target.result as string });
+            setProfileDetails((prev) => ({
+              ...prev,
+              profilePicture: {
+                id: e.target?.result as string,
+                name: file.name,
+              },
+            }));
+          };
+
+          reader.readAsDataURL(file);
+        } else {
+          setError(true);
+          setErrorMessage('Image dimensions are too large');
+        }
+      };
+
+      img.onerror = () => {
+        setError(true);
+        setErrorMessage('Invalid image');
+      };
+
+      img.src = URL.createObjectURL(file);
+    } else {
+      setError(true);
+      setErrorMessage('Invalid file type');
+    }
+  };
+
+  const triggerFileUpload = () => {
+    if (!inputRef.current) return;
+
+    inputRef.current.click();
+  };
+
+  const handleClose = () => setError(false);
+
+  const renderImageUploadField = () => (
+    <StyledImageUploadWrapper>
+      <StyledImageUpload onClick={triggerFileUpload}>
+        {!imageData?.id ? (
+          <StyledPreImageUploadWrapper>
+            <img src={uploadImageIcon} alt='Upload Image Icon' />
+            <p>+ Upload Image</p>
+          </StyledPreImageUploadWrapper>
+        ) : (
+          <>
+            <StyledUploadedImage>
+              <img
+                src={imageData.id}
+                className='uploaded-image'
+                alt='Profile Picture'
+              />
+            </StyledUploadedImage>
+            <StyledPreImageUploadWrapper className='image-wrapper'>
+              <Svg url={uploadImageIcon} />
+              <p>Change Image</p>
+            </StyledPreImageUploadWrapper>
+          </>
+        )}
+
+        <input type='file' ref={inputRef} onChange={handleFileChange} />
+
+        {error && (
+          <Snackbar open={error} autoHideDuration={6000} onClose={handleClose}>
+            <Alert
+              onClose={handleClose}
+              severity='error'
+              sx={{ width: '100%' }}
+            >
+              {errorMessage}
+            </Alert>
+          </Snackbar>
+        )}
+      </StyledImageUpload>
+      <p>{imageData?.name}</p>
+    </StyledImageUploadWrapper>
+  );
+
   return (
     <>
       <h2>Profile Details</h2>
@@ -21,7 +130,7 @@ const ImageUploadField = ({ imageData, setImageData }: Props) => {
       <StyledProfilePictureCard>
         <StyledCardHeading>Profile picture</StyledCardHeading>
         <UploadedImageWrapper>
-          <ImageUpload imageData={imageData} setImageData={setImageData} />
+          {renderImageUploadField()}
           <p>Image must be below 1024x1024px. Use PNG or JPG format.</p>
         </UploadedImageWrapper>
       </StyledProfilePictureCard>
--- client/src/components/ImageUploadField/style.ts
+++ client/src/components/ImageUploadField/style.ts
@@ -40,3 +40,96 @@ export const UploadedImageWrapper = styled.div`
     max-width: 360px;
   }
 `;
+
+export const StyledImageUploadWrapper = styled.div`
+  margin: 1rem 0 1.5rem 0;
+
+  > p {
+    color: ${themeColors.darkGray};
+    margin-top: 2px;
+    line-height: 125%;
+    font-size: 12px;
+    max-width: 200px;
+    white-space: nowrap;
+    overflow-wrap: break-word;
+    text-overflow: ellipsis;
+    overflow: hidden;
+  }
+
+  @media screen and (min-width: ${Breakpoints.Tablet}) {
+    margin: 0;
+  }
+`;
+
+export const StyledImageUpload = styled.div`
+  border-radius: 0.75rem;
+  background-color: ${themeColors.lightLavender};
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 0.5rem;
+  position: relative;
+  overflow: hidden;
+
+  height: 200px;
+  width: 200px;
+
+  :hover {
+    background-color: ${themeColors.lavender};
+    cursor: pointer;
+
+    .image-wrapper {
+      z-index: 2;
+      background-color: rgba(0, 0, 0, 0.5);
+
+      p {
+        color: #fff;
+      }
+    }
+  }
+
+  && p {
+    color: ${themeColors.indigo};
+    font-weight: 600;
+  }
+
+  img {
+    width: 40px;
+  }
+
+  input {
+    display: none;
+  }
+
+  @media screen and (min-width: ${Breakpoints.Tablet}) {
+    margin: 0;
+  }
+`;
+
+export const StyledPreImageUploadWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  position: absolute;
+  height: 100%;
+  width: 100%;
+
+  svg path {
+    fill: #fff;
+  }
+`;
+
+export const StyledUploadedImage = styled.div`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  z-index: 1;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
